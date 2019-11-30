9c235c922be31e70cb483de283def09f1c3b66bb
Sabarish Hariraj <sabarishraj88@gmail.com>
2019-11-30 01:24:57 -0500

    Updates

---
+++ precache-manifest.38d2d023185ce78e605c4062206f1d3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92d620c168b7b6f381c9719ce70589f1",
+    "url": "/SabGalaxy/randomquotemachine.git/index.html"
+  },
+  {
+    "revision": "2532da09b6c259a6f1e7",
+    "url": "/SabGalaxy/randomquotemachine.git/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "bc7c61cb7518a584703f",
+    "url": "/SabGalaxy/randomquotemachine.git/static/css/main.5a0b5173.chunk.css"
+  },
+  {
+    "revision": "2532da09b6c259a6f1e7",
+    "url": "/SabGalaxy/randomquotemachine.git/static/js/2.9a79adc1.chunk.js"
+  },
+  {
+    "revision": "bc7c61cb7518a584703f",
+    "url": "/SabGalaxy/randomquotemachine.git/static/js/main.d99cb3e7.chunk.js"
+  },
+  {
+    "revision": "50cbd75d47d765b3ebb1",
+    "url": "/SabGalaxy/randomquotemachine.git/static/js/runtime-main.4b916de9.js"
+  }
+]);
